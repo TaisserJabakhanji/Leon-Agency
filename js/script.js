f3a24d36806d7f0dc428eb843b10d71e806a4b01
TaisserJabakhanji <taisserjabakhanji@gmail.com>
2022-11-29 20:21:12 +0300

    Dark-theme Added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function linksMenu() {
+    var x = document.getElementById("links");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
