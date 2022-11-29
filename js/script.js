b60d17a022ee65d85773d4c3e61b187a0438d055
TaisserJabakhanji <taisserjabakhanji@gmail.com>
2022-11-29 23:15:29 +0300

    Add Local Storage For Themes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Add Click For Burger Menu
 function linksMenu() {
     var x = document.getElementById("links");
     if (x.style.display === "none") {
@@ -6,3 +7,29 @@ function linksMenu() {
       x.style.display = "none";
     }
 }
+
+// Use Local Storage To remember user choice for Themes
+const colorTheme = document.querySelectorAll('[name="theme"]');
+
+// Store Theme
+const storeTheme = function(theme) {
+  localStorage.setItem("theme", theme);
+};
+
+const retrieveTheme = function() {
+  const activeTheme = localStorage.getItem("theme");
+
+  colorTheme.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    };
+  });
+};
+
+colorTheme.forEach(themeOption => {
+  themeOption.addEventListener('click', () => {
+    storeTheme(themeOption.id);
+  });
+});
+
+document.onload = retrieveTheme();
